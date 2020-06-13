f7b23ab92f85cff6fc8ab69233d5c9bc0c56602a
James Chhun <James_Chhun@intuit.com>
2020-06-13 10:50:31 -0700

    feat(dev-setup): add commitlint stylelint

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,4 +1,8 @@
-// Enforce conventional commit messages
+/**
+ * Enforce conventional commit messages
+ * https://www.npmjs.com/package/@commitlint/config-conventional
+ * */
+
 module.exports = {
   extends: ['@commitlint/config-conventional'],
 };
