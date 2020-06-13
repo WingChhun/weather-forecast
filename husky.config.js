c2c8aa5469f28221b6ea2d9248663bcf0b6d8708
James Chhun <James_Chhun@intuit.com>
2020-06-13 10:51:24 -0700

    chore(dev-setup): update comments

--- husky.config.js
+++ husky.config.js
@@ -1,9 +1,9 @@
 module.exports = {
-  // TODO: update hooks as necesarty
+  // TODO: hook in Cypress
+  // TODO: separate cypress and jest tests
   hooks: {
     'commit-msg': 'commitlint -e $GIT_PARAMS',
     'pre-commit': 'lint-staged',
-    // TODO: Run cypress and jest tests on psuh
-    // 'pre-push': 'yarn lint && yarn test',
+    'pre-push': 'yarn lint && yarn test',
   },
 };
